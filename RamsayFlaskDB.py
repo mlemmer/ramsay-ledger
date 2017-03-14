ebd3a650a9e0fbe5d1fc2182783feb5c2d65bb1d
Morgan Lemmer-Webber <mlemmer@wisc.edu>
2017-03-14 15:10:32 -0500

    various changes including adding documents tab and sorting departments

--- RamsayFlaskDB.py
+++ RamsayFlaskDB.py
@@ -20,12 +20,19 @@ def about():
 
 @app.route('/departments/')
 def departments():
+    def title_row(row):
+        return row['title']
     dept_db = departmentize_db(object_map.values())
-    return render_template('departments.html', departments=dept_db)
+    return render_template('departments.html', departments=dept_db,
+                           sorted=sorted, title_row=title_row)
 
 @app.route('/wishlists/')
 def wishlists():
     return render_template('wishlists.html')
+
+@app.route('/documents/')
+def documents():
+    return render_template('documents.html')
 
 
 if __name__ == '__main__':
